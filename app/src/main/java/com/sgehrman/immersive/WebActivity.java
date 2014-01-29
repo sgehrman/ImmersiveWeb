0b57793cc76eae1b9d0424393708760662c3ae74
Steve Gehrman <sgehrman@gmail.com>
2014-01-29 04:39:33 -0800

    s

--- app/src/main/java/com/sgehrman/immersive/WebActivity.java
+++ app/src/main/java/com/sgehrman/immersive/WebActivity.java
@@ -31,18 +31,18 @@ public class WebActivity extends Activity {
   @Override
   public void onResume() {
     super.onResume();
-boolean kitkat = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT);
-    int newUiOptions=0;
+    boolean kitkat = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT);
+    int newUiOptions = 0;
 
-    if (kitkat){
+    if (kitkat) {
       newUiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-        View.SYSTEM_UI_FLAG_FULLSCREEN |
-        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+          View.SYSTEM_UI_FLAG_FULLSCREEN |
+          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
 //        View.SYSTEM_UI_FLAG_IMMERSIVE |
 //        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
 //        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
 
-        0;
+          0;
     } else {
       newUiOptions = View.SYSTEM_UI_FLAG_LOW_PROFILE |
 
